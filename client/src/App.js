423db18e31f9af4e3e5db2fe79a9689fd79fb08c
SIRLIN JEONG <sirlinjeong@gmail.com>
2022-07-23 13:51:29 -0400

    create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="">amazona</a>
+      </header>
+      list of items
+    </div>
+  );
+}
+
+export default App;
